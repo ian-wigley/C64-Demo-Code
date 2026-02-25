2fe05eabc5337343cc7533e83884061226429d2f
Ian Wigley <ian.wigley@outlook.com>
2026-02-25 18:10:39 +0000

    Create main.asm

---
+++ Demo-Code/Trantor-Cracktro/src/main.asm
@@ -0,0 +1,7 @@
+; ******** Trantor Cracktro ******** ;
+;                                    ;
+;          SYS 2304 to start         ;
+;                                    ;
+; ********************************** ;
+                *=$0800
+                !binary "./binaries/memory-0800-3000.bin"
