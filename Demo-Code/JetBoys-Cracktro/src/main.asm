35247eac9a948c76e36e7379c4093dad7a4957c7
Ian Wigley <ian.wigley@outlook.com>
2026-02-25 18:11:18 +0000

    Create main.asm

---
+++ Demo-Code/JetBoys-Cracktro/src/main.asm
@@ -0,0 +1,7 @@
+; ******** JetBoys Cracktro ******** ;
+;                                    ;
+;          SYS 2688 to start         ;
+;                                    ;
+; ********************************** ;
+                *=$0800
+                !binary "./binaries/memory-0800-3000.bin"
