34c50741801f2a068841c32c0be5337a95ea0abd
Ian Wigley <ian.wigley@outlook.com>
2026-03-20 17:44:11 +0000

    Update main.asm

--- Demo-Code/Generic-3532/Part-3/src/main.asm
+++ Demo-Code/Generic-3532/Part-3/src/main.asm
@@ -3,5 +3,10 @@
 ;          SYS 4096 to start         ;
 ;                                    ;
 ; ********************************** ;
-                *=$0800
-                !binary "./binaries/memory-0800-ffff.bin"
+;                *=$0800
+;                !binary "./binaries/memory-0800-ffff.bin"
+
+                *=$1000
+                !binary "./binaries/memory-1000-4000.bin"
+                *=$e000
+                !binary "./binaries/memory-e000-ffff.bin"
