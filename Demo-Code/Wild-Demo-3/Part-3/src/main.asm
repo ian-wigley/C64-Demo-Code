c78c224bde6da573a1d15ec5ed4b393688278026
Ian Wigley <ian.wigley@outlook.com>
2026-04-06 17:46:09 +0100

    Update main.asm

--- Demo-Code/Wild-Demo-3/Part-3/src/main.asm
+++ Demo-Code/Wild-Demo-3/Part-3/src/main.asm
@@ -4,4 +4,7 @@
 ;                                    ;
 ; ********************************** ;
                 *=$0800
-                !binary "./binaries/memory-0800-a000.bin"
+                ;!binary "./binaries/memory-0800-a000.bin"
+                !binary "./binaries/memory-0800-5600.bin"
+                *=$9100
+                !binary "./binaries/memory-9100-a000.bin"                
